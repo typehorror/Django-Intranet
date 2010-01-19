a6f01101744436e1905cb7ccf965d1f4e07b5cd4
Brice Leroy <brice@ubuntuserver64.(none)>
2010-01-18 16:18:22 -0800

    Remoce djangodblog folder and add django attachement app

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# Django settings for intranet project.
+# Django settings for betelnutz project.
 from settings_local import *
 import os
 
@@ -6,18 +6,13 @@ PROJECT_PATH=os.path.dirname(os.path.abspath(__file__))
 
 
 MANAGERS = ADMINS
-
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Los_Angeles'
+TEMPLATE_DEBUG = DEBUG
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
@@ -28,13 +23,11 @@ USE_I18N = True
 STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -48,25 +41,22 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.load_template_source',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'common.context_processor.static_processor',
+    )
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'djangodblog.DBLogMiddleware',
+    'djangodblog.middleware.DBLogMiddleware',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "common.context_processor.static_processor",
-    )
-
 ROOT_URLCONF = 'urls'
-AUTH_PROFILE_MODULE = 'profile.profile'
-LOGIN_REDIRECT_URL = '/profile/me/'
-LOGIN_URL= '/profile/login/'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -75,16 +65,24 @@ TEMPLATE_DIRS = (
     os.path.join(PROJECT_PATH, 'templates'),
 )
 
+# Save thumbnail images to a directory directly off MEDIA_ROOT, still
+# keeping the relative directory structure of the source image.
+# Result: MEDIA_ROOT + 'thumbs/photos/1_jpg_150x150_q85.jpg'
+THUMBNAIL_BASEDIR = 'thumbs'
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
-    'common',
-    'south',
+    'django.contrib.admindocs',
     'djangodblog',
+    'sorl.thumbnail',
+    'common',
     'mailer',
-    'notification',
-    'profile',
+    'tagging',
+    'django_extensions',
+    'attachments',
 )
+
--- urls.py
+++ urls.py
@@ -1,17 +1,24 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^intranet/', include('intranet.foo.urls')),
+    (r'^attachments/', include('attachments.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('django.views.generic.simple',
+    (r'^$', 'direct_to_template', {'template': 'base.html'}),
+    #url(r'^$', 'direct_to_template', {'template': 'construct.html'}, name='under_construction'),
+    url(r'^about_me/$', 'direct_to_template', {'template': 'about_me.html', 'extra_context':{'current': 'about'}}, name='about_me'),
+    )   
+
